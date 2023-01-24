6b09226f2c65e8adacf863d4324e4c9c543515f3
Vũ Nhật Vượng <97227334+vnvdev@users.noreply.github.com>
2023-01-24 08:31:22 +0700

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -29,22 +29,21 @@ ohlc = {
 
 signal = [0]*len(df)
 
-for row in df.itertuples():
-    if row.id > 100000:
-        h1 = Data(df[:row.id].resample("1h", offset=0).apply(ohlc).dropna())
-        h4 = Data(df[:row.id].resample("4h", offset=0).apply(ohlc).dropna()) 
-        d1 = Data(df[:row.id].resample("d", offset=0).apply(ohlc).dropna()) 
-        w1 = Data(df[:row.id].resample("w", offset=0).apply(ohlc).dropna())
-        if (all(w1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(h1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(h4.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(d1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])):
-                signal[i] = 1
-        elif (all(w1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(h1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(h4.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
-            and all(d1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])):
-                signal[i] = 2
+for i in range(100000, len(df):
+    h1 = Data(df[:i].resample("1h", offset=0).apply(ohlc).dropna())
+    h4 = Data(df[:i].resample("4h", offset=0).apply(ohlc).dropna()) 
+    d1 = Data(df[:i].resample("d", offset=0).apply(ohlc).dropna()) 
+    w1 = Data(df[:i].resample("w", offset=0).apply(ohlc).dropna())
+    if (all(w1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(h1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(h4.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(d1.iloc[-1][col] > 80 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])):
+            signal[i] = 1
+    elif (all(w1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(h1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(h4.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])
+        and all(d1.iloc[-1][col] < 20 for col in ['RSI1', 'RSI2', 'RSI3', 'RSI4', 'RSI5'])):
+            signal[i] = 2
 
 df['signal'] = signal
 df = df.reset_index()
